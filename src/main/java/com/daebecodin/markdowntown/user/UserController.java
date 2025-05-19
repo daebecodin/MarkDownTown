9d3a983c01423f4b8e158d7c219108ca5c506415
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-05-18 23:57:48 -0700

    working api again

--- src/main/java/com/daebecodin/markdowntown/user/UserController.java
+++ src/main/java/com/daebecodin/markdowntown/user/UserController.java
@@ -1,4 +1,10 @@
 package com.daebecodin.markdowntown.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UserController {
+
 }
