6f851c2458690bbd0296e8e79726f9b5e0976d47
daebecodin <duranddyer123@gmail.com>
2025-05-21 14:55:03 -0700

    web config

---
+++ src/main/java/com/daebecodin/mdt/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.daebecodin.mdt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+
+        // prevent removal of semicolons for matrix variables
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+}
